dfb8451fec09c3765e0443fd7d9e1787ddf7be8a
Brian Rojas <marchinram@gmail.com>
2021-09-21 20:10:46 -0500

    android updates

--- android/app/src/main/java/com/rnpilgrimsample/MainApplication.java
+++ android/app/src/main/java/com/rnpilgrimsample/MainApplication.java
@@ -2,16 +2,18 @@ package com.rnpilgrimsample;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.foursquare.pilgrim.LogLevel;
+import com.foursquare.pilgrim.PilgrimSdk;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.foursquare.pilgrim.PilgrimSdk;
-import com.foursquare.pilgrim.LogLevel;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -71,13 +73,7 @@ public class MainApplication extends Application implements ReactApplication {
                 Class<?> aClass = Class.forName("com.rnpilgrimsample.ReactNativeFlipper");
                 aClass.getMethod("initializeFlipper", Context.class, ReactInstanceManager.class).invoke(null, context,
                         reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
             }
         }
